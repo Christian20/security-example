20d59d7391d4f49e5ef7f343dbd6a0c92548ead2
Christian Steia <christian_steia@yahoo.com>
2025-01-15 18:52:17 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const express = require('express');
+const helmet = require('helmet');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(helmet());
+
+app.use((req, res, next) => {
+  // Stub out missing regenerate and save functions.
+  // These don't make sense for client side sessions.
+  if (req.session && !req.session.regenerate) {
+    req.session.regenerate = (cb) => {
+      cb();
+    };
+  }
+  if (req.session && !req.session.save) {
+    req.session.save = (cb) => {
+      cb();
+    };
+  }
+  next();
+});
+
+app.get('/secret', (req, res) => {
+  return res.send('Your personal secret value is 42!');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+https.createServer({
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+}, app).listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
